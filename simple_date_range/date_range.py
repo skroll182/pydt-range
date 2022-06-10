10747eac1af34d6c7200b9e16e9d1013de18053d
Kirill Olar <kirill.olar26@gmail.com>
2022-06-10 15:24:59 +0600

    feat: replace hardcoded values with constants

--- simple_date_range/date_range.py
+++ simple_date_range/date_range.py
@@ -1,6 +1,13 @@
 from datetime import date, datetime, timedelta
 from typing import Union
 
+from simple_date_range.const import (
+    MICROSECONDS_IN_DAY,
+    MICROSECONDS_IN_HOUR,
+    MICROSECONDS_IN_MINUTE,
+    MICROSECONDS_IN_SECOND,
+)
+
 
 def date_range(
     start: Union[datetime, date],
@@ -16,10 +23,10 @@ def date_range(
     step_ts = 0
     for attr, factor in (
         ("microseconds", 1),
-        ("seconds", 1000000),
-        ("minutes", 60 * 1000000),
-        ("hours", 60 * 60 * 1000000),
-        ("days", 24 * 60 * 60 * 1000000),
+        ("seconds", MICROSECONDS_IN_SECOND),
+        ("minutes", MICROSECONDS_IN_MINUTE),
+        ("hours", MICROSECONDS_IN_HOUR),
+        ("days", MICROSECONDS_IN_DAY),
     ):
         try:
             step_ts += getattr(step, attr) * factor
