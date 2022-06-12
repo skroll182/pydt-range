13bbb910c4666149bfab2709e6bec9ddaf60466a
Kirill Olar <kirill.olar26@gmail.com>
2022-06-12 09:48:21 +0600

    feat: add support of `str` type for `start` and `end` parameters

--- simple_date_range/date_range.py
+++ simple_date_range/date_range.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime, timedelta
-from typing import Iterable, Union
+from typing import Iterable, Optional, Union
 
 from simple_date_range.const import (
     MICROSECONDS_IN_DAY,
@@ -10,14 +10,37 @@ from simple_date_range.const import (
 
 
 def date_range(
-    start: Union[datetime, date],
-    end: Union[datetime, date],
+    start: Union[datetime, date, str],
+    end: Union[datetime, date, str],
     step: timedelta = timedelta(days=1),
+    format_: Optional[str] = None,
 ) -> Iterable[datetime]:
     if type(start) == date:
         start = datetime(start.year, start.month, start.day)
+    elif type(start) == str:
+        if not format_:
+            raise ValueError(
+                "'format_' parameter should be set if type(start) == str"
+            )
+        start = datetime.strptime(start, format_)
+    elif type(start) not in (datetime, date, str):
+        raise TypeError(
+            "'start' parameter type should be one of (datetime, date, str)"
+        )
+
     if type(end) == date:
         end = datetime(end.year, end.month, end.day)
+    elif type(end) == str:
+        if not format_:
+            raise ValueError(
+                "'format_' parameter should be set if type(end) == str"
+            )
+        end = datetime.strptime(end, format_)
+    elif type(start) not in (datetime, date, str):
+        raise TypeError(
+            "'end' parameter type should be one of (datetime, date, str)"
+        )
+
     start_ts = int(start.timestamp() * 1_000_000)
     end_ts = int(end.timestamp() * 1_000_000)
     step_ts = 0
