9351e06bcf79280350ec18efd176a69f5e50160b
Kirill Olar <kirill.olar26@gmail.com>
2023-01-31 12:25:25 +0600

    feat: update tests

--- tests/test_pydt_range.py
+++ tests/test_pydt_range.py
@@ -1,41 +1,71 @@
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 
 import pytest
+from dateutil.relativedelta import relativedelta
 
-from pydt_range import __version__, datetime_range
-from pydt_range.const import (
-    MICROSECONDS_IN_DAY,
-    MICROSECONDS_IN_HOUR,
-    MICROSECONDS_IN_MILLISECOND,
-    MICROSECONDS_IN_MINUTE,
-    MICROSECONDS_IN_SECOND,
-    MICROSECONDS_IN_WEEK,
-)
+from pydt_range import __version__, datetime_range, is_infinite
 
 
 def test_version():
-    assert __version__ == "1.0.2"
+    assert __version__ == "1.1.0"
 
 
-def test_constants():
-    assert MICROSECONDS_IN_MILLISECOND == 1000
-    assert MICROSECONDS_IN_SECOND == 1e6
-    assert MICROSECONDS_IN_MINUTE == 6e7
-    assert MICROSECONDS_IN_HOUR == 3.6e9
-    assert MICROSECONDS_IN_DAY == 8.64e10
-    assert MICROSECONDS_IN_WEEK == 6.048e11
+@pytest.mark.parametrize(
+    "start,end,step,expected",
+    [
+        (
+            datetime(2022, 1, 1),
+            datetime(2022, 1, 30),
+            relativedelta(days=1),
+            False,
+        ),
+        (
+            datetime(2022, 1, 1),
+            datetime(2022, 1, 30),
+            relativedelta(days=-1),
+            True,
+        ),
+        (
+            datetime(2022, 1, 30),
+            datetime(2022, 1, 1),
+            relativedelta(days=-1),
+            False,
+        ),
+        (
+            datetime(2022, 1, 30),
+            datetime(2022, 1, 1),
+            relativedelta(days=1),
+            True,
+        ),
+    ],
+)
+def test_is_infinite(
+    start: datetime, end: datetime, step: relativedelta, expected: bool
+):
+    result = is_infinite(start, end, step)
+    assert result == expected
 
 
 @pytest.mark.parametrize(
     "start,end,step,format_",
     [
-        (datetime(2022, 1, 1), datetime(2022, 1, 30), timedelta(days=1), None),
-        (date(2022, 1, 1), datetime(2022, 1, 30), timedelta(days=1), None),
-        (datetime(2022, 1, 1), date(2022, 1, 30), timedelta(days=1), None),
-        (date(2022, 1, 1), date(2022, 1, 30), timedelta(days=1), None),
-        ("2022-01-01", datetime(2022, 1, 30), timedelta(days=1), "%Y-%m-%d"),
-        (datetime(2022, 1, 1), "2022-01-30", timedelta(days=1), "%Y-%m-%d"),
-        ("2022-01-01", "2022-01-30", timedelta(days=1), "%Y-%m-%d"),
+        (
+            datetime(2022, 1, 1),
+            datetime(2022, 1, 30),
+            relativedelta(days=1),
+            None,
+        ),
+        (date(2022, 1, 1), datetime(2022, 1, 30), relativedelta(days=1), None),
+        (datetime(2022, 1, 1), date(2022, 1, 30), relativedelta(days=1), None),
+        (date(2022, 1, 1), date(2022, 1, 30), relativedelta(days=1), None),
+        (
+            "2022-01-01",
+            datetime(2022, 1, 30),
+            relativedelta(days=1),
+            "%Y-%m-%d",
+        ),
+        (datetime(2022, 1, 1), "2022-01-30", relativedelta(days=1), "%Y-%m-%d"),
+        ("2022-01-01", "2022-01-30", relativedelta(days=1), "%Y-%m-%d"),
     ],
 )
 def test_date_range_positive_range(start, end, step, format_):
@@ -48,13 +78,28 @@ def test_date_range_positive_range(start, end, step, format_):
 @pytest.mark.parametrize(
     "start,end,step,format_",
     [
-        (datetime(2022, 1, 30), datetime(2022, 1, 1), timedelta(days=-1), None),
-        (date(2022, 1, 30), datetime(2022, 1, 1), timedelta(days=-1), None),
-        (datetime(2022, 1, 30), date(2022, 1, 1), timedelta(days=-1), None),
-        (date(2022, 1, 30), date(2022, 1, 1), timedelta(days=-1), None),
-        ("2022-01-30", datetime(2022, 1, 1), timedelta(days=-1), "%Y-%m-%d"),
-        (datetime(2022, 1, 30), "2022-01-01", timedelta(days=-1), "%Y-%m-%d"),
-        ("2022-01-30", "2022-01-01", timedelta(days=-1), "%Y-%m-%d"),
+        (
+            datetime(2022, 1, 30),
+            datetime(2022, 1, 1),
+            relativedelta(days=-1),
+            None,
+        ),
+        (date(2022, 1, 30), datetime(2022, 1, 1), relativedelta(days=-1), None),
+        (datetime(2022, 1, 30), date(2022, 1, 1), relativedelta(days=-1), None),
+        (date(2022, 1, 30), date(2022, 1, 1), relativedelta(days=-1), None),
+        (
+            "2022-01-30",
+            datetime(2022, 1, 1),
+            relativedelta(days=-1),
+            "%Y-%m-%d",
+        ),
+        (
+            datetime(2022, 1, 30),
+            "2022-01-01",
+            relativedelta(days=-1),
+            "%Y-%m-%d",
+        ),
+        ("2022-01-30", "2022-01-01", relativedelta(days=-1), "%Y-%m-%d"),
     ],
 )
 def test_date_range_negative_range(start, end, step, format_):
@@ -67,13 +112,23 @@ def test_date_range_negative_range(start, end, step, format_):
 @pytest.mark.parametrize(
     "start,end,step,format_",
     [
-        (datetime(2022, 1, 30), datetime(2022, 1, 1), timedelta(days=1), None),
-        (date(2022, 1, 30), datetime(2022, 1, 1), timedelta(days=1), None),
-        (datetime(2022, 1, 30), date(2022, 1, 1), timedelta(days=1), None),
-        (date(2022, 1, 30), date(2022, 1, 1), timedelta(days=1), None),
-        ("2022-01-30", datetime(2022, 1, 1), timedelta(days=1), "%Y-%m-%d"),
-        (datetime(2022, 1, 30), "2022-01-01", timedelta(days=1), "%Y-%m-%d"),
-        ("2022-01-30", "2022-01-01", timedelta(days=1), "%Y-%m-%d"),
+        (
+            datetime(2022, 1, 30),
+            datetime(2022, 1, 1),
+            relativedelta(days=1),
+            None,
+        ),
+        (date(2022, 1, 30), datetime(2022, 1, 1), relativedelta(days=1), None),
+        (datetime(2022, 1, 30), date(2022, 1, 1), relativedelta(days=1), None),
+        (date(2022, 1, 30), date(2022, 1, 1), relativedelta(days=1), None),
+        ("2022-01-30", datetime(2022, 1, 1), relativedelta(days=1), "%Y-%m-%d"),
+        (
+            datetime(2022, 1, 30),
+            "2022-01-01",
+            relativedelta(days=1),
+            "%Y-%m-%d",
+        ),
+        ("2022-01-30", "2022-01-01", relativedelta(days=1), "%Y-%m-%d"),
     ],
 )
 def test_date_range_positive_range_start_lt_end(start, end, step, format_):
@@ -84,13 +139,28 @@ def test_date_range_positive_range_start_lt_end(start, end, step, format_):
 @pytest.mark.parametrize(
     "start,end,step,format_",
     [
-        (datetime(2022, 1, 1), datetime(2022, 1, 30), timedelta(days=-1), None),
-        (date(2022, 1, 1), datetime(2022, 1, 30), timedelta(days=-1), None),
-        (datetime(2022, 1, 1), date(2022, 1, 30), timedelta(days=-1), None),
-        (date(2022, 1, 1), date(2022, 1, 30), timedelta(days=-1), None),
-        ("2022-01-01", datetime(2022, 1, 30), timedelta(days=-1), "%Y-%m-%d"),
-        (datetime(2022, 1, 1), "2022-01-30", timedelta(days=-1), "%Y-%m-%d"),
-        ("2022-01-01", "2022-01-30", timedelta(days=-1), "%Y-%m-%d"),
+        (
+            datetime(2022, 1, 1),
+            datetime(2022, 1, 30),
+            relativedelta(days=-1),
+            None,
+        ),
+        (date(2022, 1, 1), datetime(2022, 1, 30), relativedelta(days=-1), None),
+        (datetime(2022, 1, 1), date(2022, 1, 30), relativedelta(days=-1), None),
+        (date(2022, 1, 1), date(2022, 1, 30), relativedelta(days=-1), None),
+        (
+            "2022-01-01",
+            datetime(2022, 1, 30),
+            relativedelta(days=-1),
+            "%Y-%m-%d",
+        ),
+        (
+            datetime(2022, 1, 1),
+            "2022-01-30",
+            relativedelta(days=-1),
+            "%Y-%m-%d",
+        ),
+        ("2022-01-01", "2022-01-30", relativedelta(days=-1), "%Y-%m-%d"),
     ],
 )
 def test_date_range_negative_range_end_gt_start(start, end, step, format_):
@@ -104,7 +174,7 @@ def test_date_range_negative_range_end_gt_start(start, end, step, format_):
         (
             datetime(2022, 1, 1),
             datetime(2022, 1, 2),
-            timedelta(hours=1),
+            relativedelta(hours=1),
             24,
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 23),
@@ -112,7 +182,7 @@ def test_date_range_negative_range_end_gt_start(start, end, step, format_):
         (
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 1),
-            timedelta(minutes=1),
+            relativedelta(minutes=1),
             60,
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 0, 59),
@@ -120,7 +190,7 @@ def test_date_range_negative_range_end_gt_start(start, end, step, format_):
         (
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 0, 1),
-            timedelta(seconds=1),
+            relativedelta(seconds=1),
             60,
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 0, 0, 59),
@@ -128,15 +198,7 @@ def test_date_range_negative_range_end_gt_start(start, end, step, format_):
         (
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 0, 0, 1),
-            timedelta(milliseconds=1),
-            1000,
-            datetime(2022, 1, 1),
-            datetime(2022, 1, 1, 0, 0, 0, 999_000),
-        ),
-        (
-            datetime(2022, 1, 1),
-            datetime(2022, 1, 1, 0, 0, 1),
-            timedelta(microseconds=1),
+            relativedelta(microseconds=1),
             1_000_000,
             datetime(2022, 1, 1),
             datetime(2022, 1, 1, 0, 0, 0, 999_999),
@@ -144,10 +206,26 @@ def test_date_range_negative_range_end_gt_start(start, end, step, format_):
         (
             datetime(2022, 1, 1),
             datetime(2022, 1, 16),
-            timedelta(weeks=1),
+            relativedelta(weeks=1),
             3,
             datetime(2022, 1, 1),
             datetime(2022, 1, 15),
+        ),
+        (
+            datetime(2022, 1, 1),
+            datetime(2023, 1, 1),
+            relativedelta(months=1),
+            12,
+            datetime(2022, 1, 1),
+            datetime(2022, 12, 1),
+        ),
+        (
+            datetime(2000, 1, 1),
+            datetime(2023, 1, 1),
+            relativedelta(years=1),
+            23,
+            datetime(2000, 1, 1),
+            datetime(2022, 1, 1),
         ),
     ],
 )
